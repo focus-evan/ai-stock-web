56ba35e69e59d1c24a0d75272a6e517b6257c07c
Evan <344983176@qq.com>
2026-01-17 21:51:37 +0800

    feat: update

--- fake/async-routes.fake.ts
+++ fake/async-routes.fake.ts
@@ -1,6 +1,5 @@
-import { about, access, aiAssistant, home, outside, personalCenter, routeNest, stockData, system } from "#/src/router/extra-info";
+import { about, aiAssistant, home, personalCenter, stockData, system } from "#/src/router/extra-info";
 import { defineFakeRoute } from "vite-plugin-fake-server/client";
-import { ADMIN_TOKEN } from "./constants";
 import { resultSuccess } from "./utils";
 
 /**
@@ -168,59 +167,6 @@ const aboutRouter = {
 	},
 };
 
-const outsideRouter = {
-	path: "/outside",
-	handle: {
-		icon: "OutsidePageIcon",
-		title: "common.menu.outside",
-		order: outside,
-	},
-	children: [
-		{
-			path: "/outside/embedded",
-			handle: {
-				icon: "EmbeddedIcon",
-				title: "common.menu.embedded",
-			},
-			children: [
-				{
-					path: "/outside/embedded/ant-design",
-					handle: {
-						icon: "AntDesignOutlined",
-						title: "common.menu.antd",
-						iframeLink: "https://ant.design/",
-					},
-				},
-				{
-					path: "/outside/embedded/project-docs",
-					handle: {
-						icon: "ContainerOutlined",
-						title: "common.menu.projectDocs",
-						iframeLink: "https://condorheroblog.github.io/react-antd-admin/docs/",
-					},
-				},
-			],
-		},
-		{
-			path: "/outside/external-link",
-			handle: {
-				icon: "ExternalIcon",
-				title: "common.menu.externalLink",
-			},
-			children: [
-				{
-					path: "/outside/external-link/react-docs",
-					handle: {
-						icon: "RiReactjsLine",
-						title: "common.menu.reactDocs",
-						externalLink: "https://react.dev/",
-					},
-				},
-			],
-		},
-	],
-};
-
 const personalCenterRouter = {
 	path: "/personal-center",
 	handle: {
@@ -246,126 +192,20 @@ const personalCenterRouter = {
 	],
 };
 
-const routeNestRouter = {
-	path: "/route-nest",
-	handle: {
-		order: routeNest,
-		title: "common.menu.nestMenus",
-		icon: "NodeExpandOutlined",
-	},
-	children: [
-		{
-			path: "/route-nest/menu1",
-			handle: {
-				title: "common.menu.menu1",
-				icon: "SisternodeOutlined",
-			},
-			children: [
-				{
-					path: "/route-nest/menu1/menu1-1",
-					handle: {
-						title: "common.menu.menu1-1",
-						icon: ("SubnodeOutlined"),
-					},
-				},
-				{
-					path: "/route-nest/menu1/menu1-2",
-					handle: {
-						title: "common.menu.menu1-2",
-						icon: ("SubnodeOutlined"),
-					},
-				},
-			],
-		},
-		{
-			path: "/route-nest/menu2",
-			handle: {
-				title: "common.menu.menu2",
-				icon: "SubnodeOutlined",
-			},
-		},
-	],
-};
-
 export default defineFakeRoute([
 	{
 		url: "/get-async-routes",
 		timeout: 1000,
 		method: "get",
-		response: ({ headers }) => {
-			const userToken = headers.authorization?.split(" ")?.[1];
-			const isAdmin = userToken === ADMIN_TOKEN;
-			const accessRouter = {
-				path: "/access",
-				handle: {
-					icon: "SafetyOutlined",
-					title: "common.menu.access",
-					order: access,
-				},
-				children: [
-					/**
-					 * @zh 通过接口获取路由时可见
-					 * @en Visible only when getting routes through the interface
-					 */
-					{
-						path: "/access/access-mode",
-						handle: {
-							icon: "CloudOutlined",
-							title: "common.menu.accessMode",
-						},
-					},
-					{
-						path: "/access/page-control",
-						handle: {
-							icon: "FileTextOutlined",
-							title: "common.menu.pageControl",
-						},
-					},
-					{
-						path: "/access/button-control",
-						handle: {
-							icon: "LockOutlined",
-							title: "common.menu.buttonControl",
-							permissions: isAdmin
-								? [
-									"permission:button:get",
-									"permission:button:update",
-									"permission:button:delete",
-									"permission:button:add",
-								]
-								: [
-									"permission:button:get",
-								],
-						},
-					},
-					isAdmin
-						? {
-							path: "/access/admin-visible",
-							handle: {
-								icon: "EyeOutlined",
-								title: "common.menu.adminVisible",
-							},
-						}
-						: {
-							path: "/access/common-visible",
-							handle: {
-								icon: "EyeOutlined",
-								title: "common.menu.commonVisible",
-							},
-						},
-				],
-			};
+		response: () => {
 			return resultSuccess(
 				[
 					homeRouter,
 					aiAssistantRouter,
 					stockDataRouter,
-					accessRouter,
 					aboutRouter,
 					systemManagementRouter,
-					outsideRouter,
 					personalCenterRouter,
-					routeNestRouter,
 				],
 			);
 		},
