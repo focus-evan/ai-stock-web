3ce666a16aafcbf17f7d18717d8155ace941c562
System Administrator <root@evandeMac-mini.local>
2026-03-13 21:26:00 -0700

    feat: 菜单管理

--- src/api/system/menu/types.ts
+++ src/api/system/menu/types.ts
@@ -1,5 +1,5 @@
 export interface MenuItemType {
-	parentId: string // 上级菜单 id
+	parentId: number // 上级菜单 id
 	id: number // 菜单 id
 	menuType: 0 | 1 | 2 | 3 // 菜单类型（0 代表菜单、1 代表 iframe、2 代表外链、3 代表按钮）
 	name: string // 菜单名称
@@ -13,7 +13,11 @@ export interface MenuItemType {
 	externalLink: string // 外链地址
 	hideInMenu: number // 是否在菜单中隐藏
 	ignoreAccess: number // 是否忽略权限
-	status: 1 // 状态（0 停用、1 启用）
-	createTime: number
-	updateTime: number
+	status: 0 | 1 // 状态（0 停用、1 启用）
+	permissions: string // 权限标识（逗号分隔）
+	roles: string // 角色标识（逗号分隔）
+	createTime: string // 创建时间
+	updateTime: string // 更新时间
+	children?: MenuItemType[] // 子菜单（树形结构）
 }
+
