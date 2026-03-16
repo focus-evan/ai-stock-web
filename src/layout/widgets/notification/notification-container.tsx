ecf8ffaf8d7d3cfaf4313bb4064108e73768e620
Evan <344983176@qq.com>
2026-03-16 18:42:39 +0800

    feat: update

--- src/layout/widgets/notification/notification-container.tsx
+++ src/layout/widgets/notification/notification-container.tsx
@@ -1,20 +1,19 @@
 import type { ButtonProps } from "antd";
 import type { NotificationItem } from "./types";
 
-import { fetchNotifications } from "#src/api/notifications";
-
 import { useEffect, useState } from "react";
 import { NotificationPopup } from "./index";
 
 export function NotificationContainer({ ...restProps }: ButtonProps) {
-	const [notifications, setNotifications] = useState<NotificationItem[]>([]);
+	const [notifications, _setNotifications] = useState<NotificationItem[]>([]);
 
 	useEffect(() => {
-		fetchNotifications().then((res) => {
-			setNotifications(
-				Array.from({ length: 20 }).flatMap(() => res.result),
-			);
-		});
+		// TODO: 后端暂未实现 notifications 接口，暂不请求
+		// fetchNotifications().then((res) => {
+		// 	setNotifications(
+		// 		Array.from({ length: 20 }).flatMap(() => res.result),
+		// 	);
+		// });
 	}, []);
 
 	return (
